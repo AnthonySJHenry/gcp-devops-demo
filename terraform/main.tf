5664a1ab3d98acf2a2b021af923f865c10d8d5b5
Anthony S.J. Henry <30806798+AnthonySJHenry@users.noreply.github.com>
2025-11-17 12:35:09 -0500

    start project

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,12 @@
-provider "gcp" {
-  project = "gcp-devops-${id}"
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "google" {
+  project = "gcp-devops-demo-478517"
   region  = "us-east1"
 }
